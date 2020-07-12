947f796c04f49c0983582c8756cffb2cce2e8142
Andrew Gregorovic <andrew.g.93@hotmail.com>
2020-07-12 09:22:31 +0000

    Created test question dictionary and topic selection to call the randomizer function. In randomizer() created variables required by the function, picks 10 random questions from the dictionary and prints them

--- app/main.py
+++ app/main.py
@@ -2,14 +2,22 @@ import random
 import time
 
 # takes the question dictionary for the selected topic and randomly picks 10 questions and 4 choices for each
-# returns a list of lists containing the questions, choices and the letter corresponding to the correct answers
-def randomizer(dict):
-    # using random module, pick a random question and mark it as having been picked so it can't be picked again
-    # using random module, pick 4 choices out of the 5 options in a random order
-    # ensure that the correct answer is always selected as one of the 4 choices and mark which choice is the answer
-    # repeat 10 times to get the full set of answers, their choices and the answers
-    # create a list for the questions, choice a, choice b, choice c, choice d and the answer key
-    # combine the lists into a single list and return
+# returns a list of lists containing the questions, choices and the answer key
+def randomizer(question_dict):
+
+    # creates individual empty lists that will be used to hold the corresponding random selections
+    question_list = []
+    choice_a_list = []
+    choice_b_list = []
+    choice_c_list = []
+    choice_d_list = []
+    answer_key = []
+
+    # picks 10 random questions from the dictionary passed to the function
+    random_questions = (random.sample(list(question_dict), k=10))
+
+    print(random_questions)
+
     pass
 
 # using the time taken to answer the question and if the user is correct,
@@ -33,14 +41,228 @@ def leaderboard():
     pass
 
 # dictionary used to store the possible questions and their choices
-test_dict = {}
+test_dict = {
+            "question1": {
+                "question": "this is question 1",
+                "choices": {
+                    "answer": "this is question 1 answer",
+                    "wrong1": "this is question 1 wrong choice 1",
+                    "wrong2": "this is question 1 wrong choice 2",
+                    "wrong3": "this is question 1 wrong choice 3",
+                    "wrong4": "this is question 1 wrong choice 4"
+                }
+            },
+            "question2": {
+                "question": "this is question 2",
+                "choices": {
+                    "answer": "this is question 2 answer",
+                    "wrong1": "this is question 2 wrong choice 1",
+                    "wrong2": "this is question 2 wrong choice 2",
+                    "wrong3": "this is question 2 wrong choice 3",
+                    "wrong4": "this is question 2 wrong choice 4"
+                }
+            },
+            "question3": {
+                "question": "this is question 3",
+                "choices": {
+                    "answer": "this is question 3 answer",
+                    "wrong1": "this is question 3 wrong choice 1",
+                    "wrong2": "this is question 3 wrong choice 2",
+                    "wrong3": "this is question 3 wrong choice 3",
+                    "wrong4": "this is question 3 wrong choice 4"
+                }
+            },
+            "question4": {
+                "question": "this is question 4",
+                "choices": {
+                    "answer": "this is question 4 answer",
+                    "wrong1": "this is question 4 wrong choice 1",
+                    "wrong2": "this is question 4 wrong choice 2",
+                    "wrong3": "this is question 4 wrong choice 3",
+                    "wrong4": "this is question 4 wrong choice 4"
+                }
+            },
+            "question5": {
+                "question": "this is question 5",
+                "choices": {
+                    "answer": "this is question 5 answer",
+                    "wrong1": "this is question 5 wrong choice 1",
+                    "wrong2": "this is question 5 wrong choice 2",
+                    "wrong3": "this is question 5 wrong choice 3",
+                    "wrong4": "this is question 5 wrong choice 4"
+                }
+            },
+            "question6": {
+                "question": "this is question 6",
+                "choices": {
+                    "answer": "this is question 6 answer",
+                    "wrong1": "this is question 6 wrong choice 1",
+                    "wrong2": "this is question 6 wrong choice 2",
+                    "wrong3": "this is question 6 wrong choice 3",
+                    "wrong4": "this is question 6 wrong choice 4"
+                }
+            },
+            "question7": {
+                "question": "this is question 7",
+                "choices": {
+                    "answer": "this is question 7 answer",
+                    "wrong1": "this is question 7 wrong choice 1",
+                    "wrong2": "this is question 7 wrong choice 2",
+                    "wrong3": "this is question 7 wrong choice 3",
+                    "wrong4": "this is question 7 wrong choice 4"
+                }
+            },
+            "question8": {
+                "question": "this is question 8",
+                "choices": {
+                    "answer": "this is question 8 answer",
+                    "wrong1": "this is question 8 wrong choice 1",
+                    "wrong2": "this is question 8 wrong choice 2",
+                    "wrong3": "this is question 8 wrong choice 3",
+                    "wrong4": "this is question 8 wrong choice 4"
+                }
+            },
+            "question9": {
+                "question": "this is question 9",
+                "choices": {
+                    "answer": "this is question 9 answer",
+                    "wrong1": "this is question 9 wrong choice 1",
+                    "wrong2": "this is question 9 wrong choice 2",
+                    "wrong3": "this is question 9 wrong choice 3",
+                    "wrong4": "this is question 9 wrong choice 4"
+                }
+            },
+            "question10": {
+                "question": "this is question 10",
+                "choices": {
+                    "answer": "this is question 10 answer",
+                    "wrong1": "this is question 10 wrong choice 1",
+                    "wrong2": "this is question 10 wrong choice 2",
+                    "wrong3": "this is question 10 wrong choice 3",
+                    "wrong4": "this is question 10 wrong choice 4"
+                }
+            },
+            "question11": {
+                "question": "this is question 11",
+                "choices": {
+                    "answer": "this is question 11 answer",
+                    "wrong1": "this is question 11 wrong choice 1",
+                    "wrong2": "this is question 11 wrong choice 2",
+                    "wrong3": "this is question 11 wrong choice 3",
+                    "wrong4": "this is question 11 wrong choice 4"
+                }
+            },
+            "question12": {
+                "question": "this is question 12",
+                "choices": {
+                    "answer": "this is question 12 answer",
+                    "wrong1": "this is question 12 wrong choice 1",
+                    "wrong2": "this is question 12 wrong choice 2",
+                    "wrong3": "this is question 12 wrong choice 3",
+                    "wrong4": "this is question 12 wrong choice 4"
+                }
+            },
+            "question13": {
+                "question": "this is question 13",
+                "choices": {
+                    "answer": "this is question 13 answer",
+                    "wrong1": "this is question 13 wrong choice 1",
+                    "wrong2": "this is question 13 wrong choice 2",
+                    "wrong3": "this is question 13 wrong choice 3",
+                    "wrong4": "this is question 13 wrong choice 4"
+                }
+            },
+            "question14": {
+                "question": "this is question 14",
+                "choices": {
+                    "answer": "this is question 14 answer",
+                    "wrong1": "this is question 14 wrong choice 1",
+                    "wrong2": "this is question 14 wrong choice 2",
+                    "wrong3": "this is question 14 wrong choice 3",
+                    "wrong4": "this is question 14 wrong choice 4"
+                }
+            },
+            "question15": {
+                "question": "this is question 15",
+                "choices": {
+                    "answer": "this is question 15 answer",
+                    "wrong1": "this is question 15 wrong choice 1",
+                    "wrong2": "this is question 15 wrong choice 2",
+                    "wrong3": "this is question 15 wrong choice 3",
+                    "wrong4": "this is question 15 wrong choice 4"
+                }
+            },
+            "question16": {
+                "question": "this is question 16",
+                "choices": {
+                    "answer": "this is question 16 answer",
+                    "wrong1": "this is question 16 wrong choice 1",
+                    "wrong2": "this is question 16 wrong choice 2",
+                    "wrong3": "this is question 16 wrong choice 3",
+                    "wrong4": "this is question 16 wrong choice 4"
+                }
+            },
+            "question17": {
+                "question": "this is question 17",
+                "choices": {
+                    "answer": "this is question 17 answer",
+                    "wrong1": "this is question 17 wrong choice 1",
+                    "wrong2": "this is question 17 wrong choice 2",
+                    "wrong3": "this is question 17 wrong choice 3",
+                    "wrong4": "this is question 17 wrong choice 4"
+                }
+            },
+            "question18": {
+                "question": "this is question 18",
+                "choices": {
+                    "answer": "this is question 18 answer",
+                    "wrong1": "this is question 18 wrong choice 1",
+                    "wrong2": "this is question 18 wrong choice 2",
+                    "wrong3": "this is question 18 wrong choice 3",
+                    "wrong4": "this is question 18 wrong choice 4"
+                }
+            },
+            "question19": {
+                "question": "this is question 19",
+                "choices": {
+                    "answer": "this is question 19 answer",
+                    "wrong1": "this is question 19 wrong choice 1",
+                    "wrong2": "this is question 19 wrong choice 2",
+                    "wrong3": "this is question 19 wrong choice 3",
+                    "wrong4": "this is question 19 wrong choice 4"
+                }
+            },
+            "question20": {
+                "question": "this is question 20",
+                "choices": {
+                    "answer": "this is question 20 answer",
+                    "wrong1": "this is question 20 wrong choice 1",
+                    "wrong2": "this is question 20 wrong choice 2",
+                    "wrong3": "this is question 20 wrong choice 3",
+                    "wrong4": "this is question 20 wrong choice 4"
+                }
+            }
+}
 
 # welcome/menu screen
 print("CLahoot!")
 # ask user to select topic
+print("""There are 3 topics available for you to choose between,
+1) Topic 1
+2) Topic 2
+3) Topic 3\n""")
+selected_topic = input("What topic would you like to be quizzed on? (Please enter the topic number)\n")
 # ask user to enter name
 # display rules by entering "r" during topic selection or name input
 # call randomizer() to get data for current quiz
+if selected_topic == "1":
+    current_quiz = randomizer(test_dict)
+elif selected_topic == "2":
+    current_quiz = randomizer(test_dict)
+else:
+    current_quiz = randomizer(test_dict)
+
+print(current_quiz)
 # display question 1
 # ask user for answer
 # display whether user is correct or not, display correct answer and how long it took
